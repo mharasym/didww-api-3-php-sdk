3726e2dd750618cc22f9830947f87cc815aaff29
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-23 09:44:24 +0100

    Use ResponseParser::create() factory instead of manual parser wiring

--- src/Configuration.php
+++ src/Configuration.php
@@ -36,22 +36,7 @@ class Configuration
         $typeMapper = new TypeMapper();
         $typeMapper->registerItems();
 
-        $metaParser = new \Swis\JsonApi\Client\Parsers\MetaParser();
-        $linksParser = new \Swis\JsonApi\Client\Parsers\LinksParser($metaParser);
-        $itemParser = new \Swis\JsonApi\Client\Parsers\ItemParser($typeMapper, $linksParser, $metaParser);
-        $collectionParser = new \Swis\JsonApi\Client\Parsers\CollectionParser($itemParser);
-        $errorParser = new \Swis\JsonApi\Client\Parsers\ErrorParser($linksParser, $metaParser);
-        $errorCollectionParser = new \Swis\JsonApi\Client\Parsers\ErrorCollectionParser($errorParser);
-        $jsonapiParser = new \Swis\JsonApi\Client\Parsers\JsonapiParser($metaParser);
-        $documentParser = new \Swis\JsonApi\Client\Parsers\DocumentParser(
-            $itemParser,
-            $collectionParser,
-            $errorCollectionParser,
-            $linksParser,
-            $jsonapiParser,
-            $metaParser
-        );
-        $responseParser = new \Swis\JsonApi\Client\Parsers\ResponseParser($documentParser);
+        $responseParser = \Swis\JsonApi\Client\Parsers\ResponseParser::create($typeMapper);
 
         self::$documentClient = new \Swis\JsonApi\Client\DocumentClient($client, $responseParser);
     }
